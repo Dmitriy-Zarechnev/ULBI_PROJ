160da6f82ea7dc3ff1ac8957833119c4c06806b8
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-07-18 15:41:36 +0300

    18.07 - profile page

---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export enum Currency {
+    RUB = 'RUB',
+    EUR = 'EUR',
+    USD = 'USD',
+}
+
+export enum Country {
+    Russia = 'Russia',
+    China = 'China',
+    Armenia = 'Armenia',
+    Kazakhstan = 'Kazakhstan',
+    USA = 'USA'
+}
